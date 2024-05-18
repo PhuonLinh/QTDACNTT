58d74fd83747cbc5ee04be87dd990a422cebf995
PhuonLinh <trinhphuonglinh231103@gmail.com>
2024-05-18 12:49:58 +0700

    Continue

--- screens/Cart.js
+++ screens/Cart.js
@@ -11,6 +11,7 @@ import request from '../config/axiosConfig';
 import numeral from 'numeral'
 import * as Font from 'expo-font';
 import styles from '../styles/Cart.Style';
+
 /*hoan tat thay the*/
 
 const Cart = ({ navigation }) => {
